ffd35441ed20c023e2a8b7db900326105deb32cb
Jay Figs <joeyfigaro@Jays-MacBook-Pro.local>
2024-11-18 13:41:12 -0500

    feat: move oh-my-zsh into dotfiles

---
+++ oh-my-zsh/aliases.zsh
@@ -0,0 +1,8 @@
+# git
+alias gst="git status"
+alias gaa="git add -A"
+alias gcm="git commit -m"
+alias gco="git checkout"
+alias gcb="git checkout -b"
+alias gbr="git branch"
+alias gac="git add -A && git commit -m"
